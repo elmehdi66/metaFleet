cec5cc13b31806a1b3cfb0a8ac2c97e37e5d5a86
Elmehdi Ghazri <mido.med75@gmail.com>
2023-04-08 14:42:19 +0000

    create models and controllers

---
+++ app/Http/Controllers/ConducteurController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Conducteur;
+use Illuminate\Http\Request;
+
+class ConducteurController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Conducteur $conducteur)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Conducteur $conducteur)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Conducteur $conducteur)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(Conducteur $conducteur)
+    {
+        //
+    }
+}
